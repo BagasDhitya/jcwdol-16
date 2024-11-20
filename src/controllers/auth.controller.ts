be7bff040531ff8b12a13cbaf5867f8e38227984
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-11-20 19:57:26 +0700

    feat: auth feature login

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,13 @@
+import { AuthService } from "../services/auth.service";
+
+export class AuthController {
+  private authService: AuthService;
+
+  constructor() {
+    this.authService = new AuthService();
+  }
+
+  async login() {
+    return this.authService.login();
+  }
+}
